97b853d72a5093fc76c0a55a1e59a90be8352d62
Cappors <cappors@gmail.com>
2018-10-31 15:53:30 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next)=> {
+    res.send('<h1>Hello from Express!</h1>')
+});
+
+const server = http.createServer(app);
+
+server.listen(3000);
