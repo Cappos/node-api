1cd3a312a298bb26d0fe9f48cbf3be2cba5f8088
Cappors <cappors@gmail.com>
2018-11-05 13:50:21 +0100

    session finished

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const MONGO_URI = 'mongodb://127.0.0.1:27017/node-complete';
 
@@ -12,27 +14,38 @@ const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const app = express();
+const store = new MongoDBStore({
+    uri: MONGO_URI,
+    collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRouts = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({secret: 'my secret', resave: false, saveUninitialized: false, store: store}));
+
 app.use((req, res, next) => {
-    User.findById("5be015596497b713fc256785").then(user => {
-        req.user = user;
-        next();
-    }).catch(err => {
-        console.log(err);
-    });
+    if (!req.session.user) {
+        return next();
+    }
+    User.findById(req.session.user._id)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRouts);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
