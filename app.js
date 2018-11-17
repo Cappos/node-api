baebf355d76ba2fbe6f3cd7714db7fe73ad6130b
cappors@gmail.com <konjuktivitis84>
2018-11-17 13:34:32 +0100

    Files started

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
-const flash =require('connect-flash');
+const flash = require('connect-flash');
+const multer = require('multer');
 
 const MONGO_URI = 'mongodb://127.0.0.1:27017/node-complete';
 
@@ -23,6 +24,23 @@ const store = new MongoDBStore({
 
 const csrfProtection = csrf();
 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images')
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString() + '_' + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -31,7 +49,9 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use(session({secret: 'my secret', resave: false, saveUninitialized: false, store: store}));
 
@@ -50,14 +70,14 @@ app.use((req, res, next) => {
     }
     User.findById(req.session.user._id)
         .then(user => {
-            if(!user){
-             return next();
+            if (!user) {
+                return next();
             }
             req.user = user;
             next();
         })
         .catch(err => {
-           next(new Error(err));
+            next(new Error(err));
         });
 });
 
@@ -65,11 +85,11 @@ app.use('/admin', adminRouts);
 app.use(shopRoutes);
 app.use(authRoutes);
 
-app.get('/500',errorController.get500);
+app.get('/500', errorController.get500);
 
 app.use(errorController.get404);
 
-app.use((error, req, res, next)=> {
+app.use((error, req, res, next) => {
     res.status(500).render('500', {
         pageTitle: 'Error',
         path: '/500',
--- controllers/admin.js
+++ controllers/admin.js
@@ -15,9 +15,27 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
     const title = req.body.title;
-    const imageUrl = req.body.imageUrl;
+    const image = req.file;
     const price = req.body.price;
     const description = req.body.description;
+
+    if(!image){
+        return res.status(422).render('admin/edit-product', {
+            pageTitle: 'Add Product',
+            path: '/admin/add-product',
+            editing: false,
+            hasError: true,
+            product: {
+                title: title,
+                price: price,
+                description: description
+            },
+            errorMessage: 'File not image',
+            validationErrors: []
+        });
+    }
+
+    const imageUrl = image.filePath;
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -29,7 +47,6 @@ exports.postAddProduct = (req, res, next) => {
             hasError: true,
             product: {
                 title: title,
-                imageUrl: imageUrl,
                 price: price,
                 description: description
             },
@@ -106,7 +123,7 @@ exports.postEditProduct = (req, res, next) => {
     const prodId = req.body.productId;
     const updatedTitle = req.body.title;
     const updatedPrice = req.body.price;
-    const updatedImageUrl = req.body.imageUrl;
+    const image = req.file;
     const updatedDesc = req.body.description;
 
     const errors = validationResult(req);
@@ -119,7 +136,6 @@ exports.postEditProduct = (req, res, next) => {
             hasError: true,
             product: {
                 title: updatedTitle,
-                imageUrl: updatedImageUrl,
                 price: updatedPrice,
                 description: updatedDesc,
                 _id: prodId
@@ -137,7 +153,9 @@ exports.postEditProduct = (req, res, next) => {
             product.title = updatedTitle;
             product.price = updatedPrice;
             product.description = updatedDesc;
-            product.imageUrl = updatedImageUrl;
+            if(image){
+                product.imageUrl = image.filePath;
+            }
             return product.save().then(result => {
                 console.log('UPDATED PRODUCT!');
                 res.redirect('/admin/products');
